eaab2379e75384c73525920a1f98eb2d94e61435
Oskar Strom <oskarestrom@protonmail.com>
2022-10-26 17:24:42 +0200

    added git to url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description='ND2 File Handling',
     # long_description=long_description,
     # long_description_content_type="text/markdown",
-    url='https://github.com/oskarstromski/nd2handling',
+    url='https://github.com/oskarstromski/nd2handling.git',
     # project_urls = {
     #     "Bug Tracker": ""
     # },
